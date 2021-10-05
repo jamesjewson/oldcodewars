d7fc856967c48ade833bbcfd92b5f0267df93ecd
James Jewson <jamesjewson.dev@gmail.com>
2021-10-05 10:30:18 -0500

    added kata

--- codeCatalog/main.js
+++ codeCatalog/main.js
@@ -20,3 +20,32 @@ function alphabetPosition(text) {
     text = newerArr.join(' ')
     return text;
   }
+
+
+
+//////////////////////////////////////////////////////
+
+
+  // A wealthy client has forgotten the password to his business website, but he has a list of possible passwords. His previous developer has left a file on the server with the name password.txt. You open the file and realize it's in binary format.
+
+  // Write a script that takes an array of possible passwords and a string of binary representing the possible password. Convert the binary to a string and compare to the password array. If the password is found, return the password string, else return false;
+  
+  // decodePass(['password123', 'admin', 'admin1'], '01110000 01100001 01110011 01110011 01110111 01101111 01110010 01100100 00110001 00110010 00110011');    => 'password123'
+  // decodePass(['password321', 'admin', 'admin1'], '01110000 01100001 01110011 01110011 01110111 01101111 01110010 01100100 00110001 00110010 00110011');    => false
+  // decodePass(['password456', 'pass1', 'test12'], '01110000 01100001 01110011 01110011 01110111 01101111 01110010 01100100 00110001 00110010 00110011');    => false
+
+
+  function decodePass( passArr, bin ){
+    //convert the binary to characters and join as a string
+      bin = bin.split(' ')
+      let converted = bin.map(elem => String.fromCharCode(parseInt(elem, 2))).join("")
+    
+    //compare to password
+      if(passArr.includes(converted)){
+        return converted
+      }else{
+        return false
+      }
+  }
+
+  ////////////////////////////////////////////
